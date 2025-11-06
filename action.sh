0bee52fcbbca6808490d03fb7d58a5ae8dccecb3
Arkadiusz biel <bielu@bielu.com.pl>
2025-11-06 16:52:27 +0000

    create basic command

--- action.sh
+++ action.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 main(){
+	case $action in
 
-	
+  install)
+    echo -n "Installing aspire";
+    curl -sSL https://aspire.dev/install.sh | bash
+    ;
+  *)
+    echo -n "unknown action"
+    exit 1
+    ;;
+esac
+
 }
 
 main

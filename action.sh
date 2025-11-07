18e4b9756d6a4ec721b4e7a232b07fafb8af4dcc
Arkadiusz biel <bielu@bielu.com.pl>
2025-11-07 10:34:38 +0000

    implement publish method

--- action.sh
+++ action.sh
@@ -5,8 +5,24 @@ main(){
 
   install)
     echo -n "Installing aspire";
-    curl -sSL https://aspire.dev/install.sh | bash
-    ;
+    curl -sSL https://aspire.dev/install.sh | bash;
+
+    ;;
+  install)
+    echo -n "publishing aspire project";
+    cmd_string="aspire publish"
+    if [[ -n $PROJECT ]]; then
+      cmd_string += " --project $PROJECT"
+    fi
+    if [[ -n $output ]]; then
+      cmd_string += " --output-pat $output"
+    fi
+    if [[ "$DEBUG" == "true" ]]; then
+      cmd_string += " --debug"
+    fi
+    result=$($cmd_string)
+    echo $result
+    ;;
   *)
     echo -n "unknown action"
     exit 1
